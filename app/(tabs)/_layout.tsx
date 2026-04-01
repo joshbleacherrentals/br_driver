6df087ee866aa225ad841b2478666baa956b1553
Sarah Joo <79344570+sarahjoo8888@users.noreply.github.com>
2026-04-01 10:08:06 -0400

    combine history and trips into 1 tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -55,15 +55,6 @@ export default function TabLayout() {
               }}
             />
             <Tabs.Screen
-              name="completedTrips"
-              options={{
-                title: "History",
-                tabBarIcon: ({ color }) => (
-                  <FontAwesome6 name="clock-rotate-left" size={24} color={color} />
-                ),
-              }}
-            />
-            <Tabs.Screen
               name="driverAvailability"
               options={{
                 title: "Calendar",
